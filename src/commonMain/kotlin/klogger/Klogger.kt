a02dd94e8134ed61b7bf67e816a860169d1bffa9
Michael Strasser <michael@michaelstrasser.com>
2021-06-22 22:48:23 +1000

    Start of Klogger interface tests

--- src/commonMain/kotlin/klogger/Klogger.kt
+++ src/commonMain/kotlin/klogger/Klogger.kt
@@ -5,4 +5,28 @@ package klogger
  */
 interface Klogger {
 
+    fun minLevel(): Level
+    fun isLevelEnabled(level: Level) = minLevel() <= level
+    fun isTraceEnabled() = minLevel() <= Level.TRACE
+    fun isDebugEnabled() = minLevel() <= Level.DEBUG
+    fun isInfoEnabled() = minLevel() <= Level.INFO
+
+    fun log(level: Level, message: String)
+    fun trace(message: String) = log(Level.TRACE, message)
+    fun debug(message: String) = log(Level.DEBUG, message)
+    fun info(message: String) = log(Level.INFO, message)
+    fun warn(message: String) = log(Level.WARN, message)
+    fun error(message: String) = log(Level.ERROR, message)
+    fun fatal(message: String) = log(Level.FATAL, message)
+
+    fun log(level: Level, message: () -> String) {
+        if (isLevelEnabled(level)) log(level, message())
+    }
+
+    fun trace(message: () -> String) = log(Level.TRACE, message)
+    fun debug(message: () -> String) = log(Level.DEBUG, message)
+    fun info(message: () -> String) = log(Level.INFO, message)
+    fun warn(message: () -> String) = log(Level.WARN, message)
+    fun error(message: () -> String) = log(Level.ERROR, message)
+    fun fatal(message: () -> String) = log(Level.FATAL, message)
 }
---
+++ src/jvmTest/kotlin/klogger/KloggerTest.kt
@@ -0,0 +1,49 @@
+package klogger
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+fun testLogger(
+    level: Level = Level.FATAL
+) = object : Klogger {
+
+    override fun minLevel() = level
+
+    override fun log(level: Level, message: String) {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class KloggerTest : DescribeSpec({
+
+    describe("Klogger") {
+        describe("level check") {
+            it("isTraceEnabled() is true only for TRACE") {
+                testLogger(Level.TRACE).isTraceEnabled() shouldBe true
+                testLogger(Level.DEBUG).isTraceEnabled() shouldBe false
+                testLogger(Level.INFO).isTraceEnabled() shouldBe false
+                testLogger(Level.WARN).isTraceEnabled() shouldBe false
+                testLogger(Level.ERROR).isTraceEnabled() shouldBe false
+                testLogger(Level.FATAL).isTraceEnabled() shouldBe false
+            }
+            it("isDebugEnabled() is true only for TRACE and DEBUG") {
+                testLogger(Level.TRACE).isDebugEnabled() shouldBe true
+                testLogger(Level.DEBUG).isDebugEnabled() shouldBe true
+                testLogger(Level.INFO).isDebugEnabled() shouldBe false
+                testLogger(Level.WARN).isDebugEnabled() shouldBe false
+                testLogger(Level.ERROR).isDebugEnabled() shouldBe false
+                testLogger(Level.FATAL).isDebugEnabled() shouldBe false
+            }
+            it("isInfoEnabled() is true only for TRACE, DEBUG and INFO") {
+                testLogger(Level.TRACE).isInfoEnabled() shouldBe true
+                testLogger(Level.DEBUG).isInfoEnabled() shouldBe true
+                testLogger(Level.INFO).isInfoEnabled() shouldBe true
+                testLogger(Level.WARN).isInfoEnabled() shouldBe false
+                testLogger(Level.ERROR).isInfoEnabled() shouldBe false
+                testLogger(Level.FATAL).isInfoEnabled() shouldBe false
+            }
+        }
+    }
+
+})
