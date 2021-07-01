ae705f40683852cb9f42b85721106827656ae720
Michael Strasser <michael@michaelstrasser.com>
2021-07-01 15:28:33 +1000

    Improved test readability

--- src/jvmTest/kotlin/klogger/LogContextTest.kt
+++ src/jvmTest/kotlin/klogger/LogContextTest.kt
@@ -6,48 +6,44 @@ import io.kotest.matchers.shouldNotBe
 import klogger.context.LogContext
 import klogger.context.logContext
 import kotlinx.coroutines.launch
-import java.util.UUID
-
-const val CORRELATION_ID = "correlationId"
 
 class LogContextTest : DescribeSpec({
     describe("LogContext implementation of CoroutineContext") {
-        it("a context item can be retrieved in a coroutine scope") {
-            val id = UUID.randomUUID()
-            launch(logContext(CORRELATION_ID to id)) {
-                val context = coroutineContext[LogContext]
-                context shouldNotBe null
-                context?.get(CORRELATION_ID) shouldBe id
-            }
-        }
-        it("a context item cannot be retrieved outside its coroutine scope") {
-            launch(logContext()) {
-                coroutineContext[LogContext] shouldNotBe null
-            }
-            coroutineContext[LogContext] shouldBe null
-        }
-        it("a LogContext is available after an inner coroutine scope has finished") {
-            val id1 = UUID.randomUUID()
-            val id2 = UUID.randomUUID()
-            launch(logContext(CORRELATION_ID to id1)) {
-                coroutineContext[LogContext]?.get(CORRELATION_ID) shouldBe id1
-                launch(logContext(CORRELATION_ID to id2)) {
-                    coroutineContext[LogContext]?.get(CORRELATION_ID) shouldBe id2
+        describe("context items") {
+            it("can be retrieved inside the coroutine scope where it is defined") {
+                launch(logContext("colour" to "blue")) {
+                    coroutineContext[LogContext] shouldNotBe null
+                    coroutineContext[LogContext]?.get("colour") shouldBe "blue"
                 }
-                coroutineContext[LogContext]?.get(CORRELATION_ID) shouldBe id1
+            }
+            it("cannot be retrieved outside the coroutine scope where it is defined") {
+                launch(logContext("colour" to "green")) {
+                    coroutineContext[LogContext] shouldNotBe null
+                    coroutineContext[LogContext]?.get("colour") shouldBe "green"
+                }
+                coroutineContext[LogContext] shouldBe null
             }
         }
-        it("a LogContext in an inner coroutine scope contains items from a LogContext in an enclosing scope") {
-            val id = UUID.randomUUID()
-            launch(logContext(CORRELATION_ID to id)) {
-                coroutineContext[LogContext]?.get(CORRELATION_ID) shouldBe id
-                launch(logContext("this" to "that")) {
-                    val context = coroutineContext[LogContext]
-                    context?.get("this") shouldBe "that"
-                    context?.get(CORRELATION_ID) shouldBe id
+        describe("nested coroutine scopes") {
+            it("a LogContext is available after an inner coroutine scope has finished") {
+                launch(logContext("scope" to "outer")) {
+                    coroutineContext[LogContext]?.get("scope") shouldBe "outer"
+                    launch(logContext("scope" to "inner")) {
+                        coroutineContext[LogContext]?.get("scope") shouldBe "inner"
+                    }
+                    coroutineContext[LogContext]?.get("scope") shouldBe "outer"
                 }
-                coroutineContext[LogContext]?.get(CORRELATION_ID) shouldBe id
-                coroutineContext[LogContext]?.get("this") shouldBe null
+            }
+            it("a LogContext in an inner coroutine scope contains items from a LogContext in an enclosing scope") {
+                launch(logContext("scope" to "outer")) {
+                    coroutineContext[LogContext]?.get("scope") shouldBe "outer"
+                    launch(logContext("colour" to "green")) {
+                        coroutineContext[LogContext]?.get("scope") shouldBe "outer"
+                        coroutineContext[LogContext]?.get("colour") shouldBe "green"
+                    }
+                    coroutineContext[LogContext]?.get("scope") shouldBe "outer"
+                    coroutineContext[LogContext]?.get("colour") shouldBe null
+                }
             }
         }
     }
