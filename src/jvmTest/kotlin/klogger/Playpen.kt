6c937d28c2fe01368a175dd5cd289717c965fedd
Michael Strasser <michael@michaelstrasser.com>
2021-07-07 22:02:56 +1000

    Use lambda calls in playpen

--- src/jvmTest/kotlin/klogger/Playpen.kt
+++ src/jvmTest/kotlin/klogger/Playpen.kt
@@ -22,19 +22,19 @@ fun main() = runBlocking {
 
     val logger = BaseLogger("main")
     launch(logContext("run" to UUID.randomUUID().toString())) {
-        logger.info("Start")
+        logger.info { "Start" }
         repeat(2) { c ->
-            logger.info(">> ${c + 1}")
+            logger.info { ">> ${c + 1}" }
             launch(logContext("counter" to (c + 1).toString())) {
                 repeat(2) { i ->
-                    logger.info("Event ${i + 1} at ${LocalDateTime.now(ZoneId.of("Australia/Brisbane"))}")
+                    logger.info { "Event ${i + 1} at ${LocalDateTime.now(ZoneId.of("Australia/Brisbane"))}" }
                 }
             }
-            logger.info("<< ${c + 1}")
+            logger.info { "<< ${c + 1}" }
         }
-        logger.info("Finish")
+        logger.info { "Finish" }
     }
     // There must be at least one statement outside the coroutine scope.
-    logger.info("All done")
+    logger.info { "All done" }
 }
 
