150c7e68cd2d46dd38ea71ad331a2b07a7f88584
Michael Strasser <michael@michaelstrasser.com>
2021-07-15 21:46:22 +1000

    Bump Kotlin to 1.5.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.5.20"
-    kotlin("plugin.serialization") version "1.5.20"
+    kotlin("multiplatform") version "1.5.21"
+    kotlin("plugin.serialization") version "1.5.21"
 }
 
 group = "ktlogging"
