7cdfb8ff41f4319ea853901a822cbd78ad9d4ec3
Michael Strasser <michael@michaelstrasser.com>
2021-07-16 23:46:34 +1000

    Removed GlobalScope

--- src/commonMain/kotlin/ktlogging/Logging.kt
+++ src/commonMain/kotlin/ktlogging/Logging.kt
@@ -1,16 +1,15 @@
 package ktlogging
 
-import ktlogging.Dispatcher.dispatchEvent
-import ktlogging.events.LogEvent
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
+import ktlogging.Dispatcher.dispatchEvent
+import ktlogging.events.LogEvent
 
 /**
  * The main object for managing log event processing.
  */
-@DelicateCoroutinesApi
 object Logging {
     /**
      * [Channel] between the coroutines where log events are sent and the coroutines that send them out.
@@ -20,8 +19,7 @@ object Logging {
     /** Creates the channel and starts the loop to process the log events. */
     private fun startEventsChannel(): Channel<LogEvent> {
         val eventsChannel = Channel<LogEvent>()
-        // Use GlobalScope because the channel should exist as long as the runtime is active.
-        GlobalScope.launch {
+        CoroutineScope(Job()).launch {
             for (logEvent in eventsChannel) {
                 dispatchEvent(logEvent)
             }
