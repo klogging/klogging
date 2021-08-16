22fa0e1e60cd4d94742c456941e3748f1237d450
Michael Strasser <michael@michaelstrasser.com>
2021-08-16 22:43:18 +1000

    Debug logging when mapSupplier set

--- src/jvmMain/kotlin/io/klogging/events/runtimeContextItems.kt
+++ src/jvmMain/kotlin/io/klogging/events/runtimeContextItems.kt
@@ -18,12 +18,21 @@
 
 package io.klogging.events
 
+import io.klogging.internal.debug
+
+internal typealias MapSupplier = () -> Map<String, String?>
+
 /**
  * EXPERIMENTAL: JVM-only object that holds a supplier of maps of context items,
  * e.g. from SLF4J MDC.
  */
 public object JvmContextItems {
-    public var mapSupplier: () -> Map<String, String?> = { mapOf() }
+    internal var mapSupplier: MapSupplier = { mapOf() }
+
+    public fun setMapSupplier(supplier: MapSupplier) {
+        debug("Setting mapSupplier with ${System.identityHashCode(supplier)}")
+        mapSupplier = supplier
+    }
 }
 
 /** Contextual items in the current runtime. */
