27e1c716a7a694ebf18ee66c04814078a6139829
Michael Strasser <michael@michaelstrasser.com>
2021-08-22 06:53:14 +1000

    Dokka as GitHub pages

---
+++ scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":dokkaHtml/commonMain":[],":dokkaHtml/jsMain":[":dokkaHtml/commonMain"],":dokkaHtml/jvmMain":[":dokkaHtml/commonMain"],":dokkaHtml/nativeMain":[":dokkaHtml/commonMain"]}'
