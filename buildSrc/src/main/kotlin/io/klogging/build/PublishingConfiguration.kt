2023fe3b25a48ef267f153df49e752bf4b73c98a
Michael Strasser <michael@michaelstrasser.com>
2021-08-21 21:23:47 +1000

    Manually include `jvmSourcesJar` in publication

--- buildSrc/src/main/kotlin/io/klogging/build/PublishingConfiguration.kt
+++ buildSrc/src/main/kotlin/io/klogging/build/PublishingConfiguration.kt
@@ -83,6 +83,7 @@ private fun Project.createPublishingTasks(
                 archiveBaseName.set("slf4j-klogging-$publicationName")
             }
 
+            artifact(tasks.named("jvmSourcesJar"))
             artifact(javadocTask)
 
             pom {
