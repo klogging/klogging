4eb3e9961339a621487bdc035cebe36de4f81e45
Michael Strasser <michael@michaelstrasser.com>
2021-09-27 11:06:17 +1000

    Robust batching that does not miss events

--- src/commonMain/kotlin/io/klogging/sending/Batching.kt
+++ src/commonMain/kotlin/io/klogging/sending/Batching.kt
@@ -18,28 +18,35 @@
 
 package io.klogging.sending
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.coroutines.channels.onClosed
+import kotlinx.coroutines.channels.onFailure
+import kotlinx.coroutines.channels.onSuccess
+import kotlinx.coroutines.selects.whileSelect
 
 /**
  * Receive a batch of items from a channel, up to time and batch size limits.
  *
  * @param channel the channel to receive items from
  * @param maxTimeMs maximum time in milliseconds to wait for items
- * @param maxSize maximum batch size
  *
  * @return the list of items in the order received
  */
+@OptIn(ExperimentalCoroutinesApi::class)
 public suspend fun <E> receiveBatch(
     channel: ReceiveChannel<E>,
     maxTimeMs: Long,
     maxSize: Int,
 ): List<E> {
     val items = mutableListOf<E>()
-    withTimeoutOrNull(maxTimeMs) {
-        for (item in channel) {
-            items.add(item)
-            if (items.size >= maxSize) break
+    whileSelect {
+        onTimeout(maxTimeMs) { false }
+        channel.onReceiveCatching { result ->
+            result.onFailure { if (it != null) throw it }
+                .onClosed { return@onReceiveCatching false }
+                .onSuccess { items += it }
+            items.size < maxSize
         }
     }
     return items
