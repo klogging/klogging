621f002a52b125de730ba03650a159c3481e2e59
Michael Strasser <michael@michaelstrasser.com>
2021-11-06 20:25:47 +1000

    Separate RENDER_ECS renderer

--- src/commonMain/kotlin/io/klogging/sending/SendElk.kt
+++ src/commonMain/kotlin/io/klogging/sending/SendElk.kt
@@ -19,8 +19,7 @@
 package io.klogging.sending
 
 import io.klogging.events.LogEvent
-import io.klogging.rendering.evalTemplate
-import io.klogging.rendering.serializeMap
+import io.klogging.rendering.RENDER_ECS
 import kotlinx.serialization.Serializable
 
 /** Model of an ELK server endpoint */
@@ -36,40 +35,4 @@ public data class ElkEndpoint(
 public expect fun elkSender(endpoint: ElkEndpoint): EventSender
 
 internal fun elkBatch(batch: List<LogEvent>): String = batch
-    .joinToString("\n") { ecsEvent(it) }
-
-/**
- * Convert a [LogEvent] into JSON in
- * [Elastic Common Schema](https://www.elastic.co/guide/en/ecs/current/index.html) for sending
- * directly to an [ELK](https://www.elastic.co/what-is/elk-stack) stack.
- *
- * It populates ECS fields as follows:
- *
- * - `timestamp` -> `@timestamp`
- * - `host` -> `host.name`
- * - `logger` -> `log.logger`
- * - `level` -> `log.level`
- * - `message` (evaluated template) -> `message`
- * - If `stackTrace` is present:
- *   - `stackTrace` -> `error.stack_trace`
- *   - `message` (evaluated template) -> `error.message`
- * - `context` (if present) -> `context` in `labels` object
- * - `items` (if present) -> `items` (object)
- */
-internal fun ecsEvent(event: LogEvent): String {
-    val eventMap: MutableMap<String, Any?> = mutableMapOf(
-        "@timestamp" to event.timestamp,
-        "host.name" to event.host,
-        "log.logger" to event.logger,
-        "log.level" to event.level.name,
-        "message" to event.evalTemplate(),
-        "error.stack_trace" to event.stackTrace,
-        "error.message" to event.stackTrace?.let { event.evalTemplate() },
-    )
-    if (event.context != null)
-        eventMap += "labels" to mapOf("context" to event.context)
-    if (event.items.isNotEmpty())
-        eventMap += "items" to event.items
-
-    return serializeMap(eventMap.filterValues { it != null })
-}
+    .joinToString("\n") { RENDER_ECS(it) }
--- src/jvmTest/kotlin/io/klogging/rendering/RenderEcsTest.kt
+++ src/jvmTest/kotlin/io/klogging/rendering/RenderEcsTest.kt
@@ -16,7 +16,7 @@
 
 */
 
-package io.klogging.sending
+package io.klogging.rendering
 
 import io.klogging.Level.INFO
 import io.klogging.logEvent
@@ -24,49 +24,49 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.string.shouldContain
 import kotlinx.datetime.Instant
 
-class SendElkTest : DescribeSpec({
-    describe("`ecsEvent()` function") {
+class RenderEcsTest : DescribeSpec({
+    describe("`RENDER_ECS()` function") {
         it("includes ECS Base field `@timestamp`") {
             val ts = Instant.parse("2021-09-29T10:08:42.123456Z")
             val event = logEvent(timestamp = ts)
-            ecsEvent(event) shouldContain """"@timestamp":"2021-09-29T10:08:42.123456Z""""
+            RENDER_ECS(event) shouldContain """"@timestamp":"2021-09-29T10:08:42.123456Z""""
         }
         it("includes ECS Base field `message` containing evaluated template") {
             val event = logEvent(
                 message = "User {userId} logged in",
                 items = mapOf("userId" to "Freddo Frog")
             )
-            ecsEvent(event) shouldContain """"message":"User Freddo Frog logged in""""
+            RENDER_ECS(event) shouldContain """"message":"User Freddo Frog logged in""""
         }
         it("includes ECS Error field `error.stack_trace` if a stack trace is present") {
             val event = logEvent(stackTrace = "Not really a stack trace")
-            ecsEvent(event) shouldContain """"error.stack_trace":"Not really a stack trace""""
+            RENDER_ECS(event) shouldContain """"error.stack_trace":"Not really a stack trace""""
         }
         it("includes ECS Error field `error.message` if a stack trace is present") {
             val event = logEvent(stackTrace = "Pretend", message = "Oh noes! Something went wrong")
-            ecsEvent(event) shouldContain """"error.message":"Oh noes! Something went wrong""""
+            RENDER_ECS(event) shouldContain """"error.message":"Oh noes! Something went wrong""""
         }
         it("includes ECS Host field `host.name`") {
             val event = logEvent(host = "local-test")
-            ecsEvent(event) shouldContain """"host.name":"local-test""""
+            RENDER_ECS(event) shouldContain """"host.name":"local-test""""
         }
         it("includes ECS Log field `log.logger`") {
             val event = logEvent(logger = "SendElkTest")
-            ecsEvent(event) shouldContain """"log.logger":"SendElkTest""""
+            RENDER_ECS(event) shouldContain """"log.logger":"SendElkTest""""
         }
         it("includes ECS Log field `log.level`") {
             val event = logEvent(level = INFO)
-            ecsEvent(event) shouldContain """"log.level":"INFO""""
+            RENDER_ECS(event) shouldContain """"log.level":"INFO""""
         }
         it("defines custom label `context` with value of `context`") {
             val event = logEvent(context = "test-context")
-            ecsEvent(event).also {
+            RENDER_ECS(event).also {
                 it shouldContain """"labels":{"context":"test-context"}"""
             }
         }
         it("defines nested object `items` with value of `items`") {
             val event = logEvent(items = mapOf("colour" to "chartreuse"))
-            ecsEvent(event).also {
+            RENDER_ECS(event).also {
                 it shouldContain """"items":{"colour":"chartreuse"}"""
             }
         }
