2cde368686ad121533083085a8189a7bcdbabbe3
Michael Strasser <michael@michaelstrasser.com>
2023-02-06 11:47:53 +1000

    Update copyright year

--- src/commonMain/kotlin/io/klogging/Klogging.kt
+++ src/commonMain/kotlin/io/klogging/Klogging.kt
@@ -1,6 +1,6 @@
 /*
 
-   Copyright 2021-2022 Michael Strasser.
+   Copyright 2021-2023 Michael Strasser.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
