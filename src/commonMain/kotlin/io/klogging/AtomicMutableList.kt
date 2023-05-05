478f0804dcd3a861ad595b8318db0cfc23cd5c51
Michael Strasser <mstrass@thoughtworks.com>
2023-05-05 17:05:33 +1000

    `AtomicMutableList` uses an atomic `List` internally

--- src/commonMain/kotlin/io/klogging/AtomicMutableList.kt
+++ src/commonMain/kotlin/io/klogging/AtomicMutableList.kt
@@ -19,6 +19,7 @@
 package io.klogging
 
 import kotlinx.atomicfu.atomic
+import kotlinx.atomicfu.update
 
 /**
  * A multiplatform, thread-safe [MutableList], implemented using AtomicFU.
@@ -27,42 +28,121 @@ internal class AtomicMutableList<E>(
     vararg elements: E
 ) : MutableList<E> {
 
-    private val list = atomic(mutableListOf(*elements))
+    private val list = atomic(listOf(*elements))
 
     override val size: Int
         get() = list.value.size
 
-    override fun clear(): Unit = list.value.clear()
+    override fun clear(): Unit = list.update { buildList { emptyList<E>() } }
 
-    override fun addAll(elements: Collection<E>): Boolean = list.value.addAll(elements)
+    override fun addAll(elements: Collection<E>): Boolean {
+        var changed = false
+        list.update { current ->
+            buildList {
+                addAll(current)
+                changed = addAll(elements)
+            }
+        }
+        return changed
+    }
 
-    override fun addAll(index: Int, elements: Collection<E>): Boolean = list.value.addAll(index, elements)
+    override fun addAll(index: Int, elements: Collection<E>): Boolean {
+        var changed = false
+        list.update { current ->
+            buildList {
+                addAll(current)
+                changed = addAll(index, elements)
+            }
+        }
+        return changed
+    }
 
-    override fun add(index: Int, element: E) = list.value.add(index, element)
+    override fun add(index: Int, element: E) = list.update { current ->
+        buildList {
+            addAll(current)
+            add(index, element)
+        }
+    }
 
-    override fun add(element: E): Boolean = list.value.add(element)
+    override fun add(element: E): Boolean {
+        list.update { current ->
+            buildList {
+                addAll(current)
+                add(element)
+            }
+        }
+        return true
+    }
 
-    override fun get(index: Int): E = list.value.get(index)
+    override fun get(index: Int): E = list.value[index]
 
     override fun isEmpty(): Boolean = list.value.isEmpty()
 
-    override fun iterator(): MutableIterator<E> = list.value.iterator()
+    override fun iterator(): MutableIterator<E> = list.value.toMutableList().iterator()
 
-    override fun listIterator(): MutableListIterator<E> = list.value.listIterator()
+    override fun listIterator(): MutableListIterator<E> = list.value.toMutableList().listIterator()
 
-    override fun listIterator(index: Int): MutableListIterator<E> = list.value.listIterator(index)
+    override fun listIterator(index: Int): MutableListIterator<E> = list.value.toMutableList().listIterator(index)
 
-    override fun removeAt(index: Int): E = list.value.removeAt(index)
+    override fun removeAt(index: Int): E {
+        checkElementIndex(index, size)
+        val removed: E = get(index)
+        list.update { current ->
+            buildList {
+                addAll(current)
+                removeAt(index)
+            }
+        }
+        return removed
+    }
 
-    override fun subList(fromIndex: Int, toIndex: Int): MutableList<E> = list.value.subList(fromIndex, toIndex)
+    override fun subList(fromIndex: Int, toIndex: Int): MutableList<E> =
+        list.value.toMutableList().subList(fromIndex, toIndex)
 
-    override fun set(index: Int, element: E): E = list.value.set(index, element)
+    override fun set(index: Int, element: E): E {
+        checkElementIndex(index, size)
+        val replaced: E = get(index)
+        list.update { current ->
+            buildList {
+                addAll(current)
+                set(index, element)
+            }
+        }
+        return replaced
+    }
 
-    override fun retainAll(elements: Collection<E>): Boolean = list.value.retainAll(elements)
+    override fun retainAll(elements: Collection<E>): Boolean {
+        var modified = false
+        list.update { current ->
+            buildList {
+                addAll(current)
+                modified = retainAll(elements)
+            }
+        }
+        return modified
+    }
 
-    override fun removeAll(elements: Collection<E>): Boolean = list.value.removeAll(elements)
+    override fun removeAll(elements: Collection<E>): Boolean {
+        var modified = false
+        list.update { current ->
+            buildList {
+                addAll(current)
+                modified = removeAll(elements)
+            }
+        }
+        return modified
+    }
 
-    override fun remove(element: E): Boolean = list.value.remove(element)
+    override fun remove(element: E): Boolean {
+        var modified = false
+        list.update { current ->
+            buildList {
+                addAll(current)
+                modified = remove(element)
+            }
+        }
+        return modified
+    }
 
     override fun lastIndexOf(element: E): Int = list.value.lastIndexOf(element)
 
@@ -71,4 +151,13 @@ internal class AtomicMutableList<E>(
     override fun containsAll(elements: Collection<E>): Boolean = list.value.containsAll(elements)
 
     override fun contains(element: E): Boolean = list.value.contains(element)
+
+    /**
+     * Copied from Kotlin AbstractList code.
+     */
+    private fun checkElementIndex(index: Int, size: Int) {
+        if (index < 0 || index >= size) {
+            throw IndexOutOfBoundsException("index: $index, size: $size")
+        }
+    }
 }
