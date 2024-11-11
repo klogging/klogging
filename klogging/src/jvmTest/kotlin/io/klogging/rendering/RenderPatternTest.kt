9eab63ac1a36f6eeabee852b7c0ec2a8e18a4d98
Michael Strasser <michael@michaelstrasser.com>
2024-11-11 22:25:14 +1000

    Simple `RenderPattern` class

---
+++ klogging/src/jvmTest/kotlin/io/klogging/rendering/RenderPatternTest.kt
@@ -0,0 +1,42 @@
+/*
+
+   Copyright 2021-2024 Michael Strasser.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       https://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+package io.klogging.rendering
+
+import io.klogging.logEvent
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class RenderPatternTest : DescribeSpec({
+    describe("RenderPattern") {
+        it("renders message and newline only by default") {
+            val event = logEvent()
+            RenderPattern()(event) shouldBe "${event.message}\n"
+        }
+        it("renders message only if %m specified") {
+            val event = logEvent()
+            RenderPattern("%m")(event) shouldBe event.message
+        }
+        it("renders complex messages") {
+            val event = logEvent()
+            RenderPattern("%t %v [%c] : %l : %m : %i%n")(event) shouldBe
+                    "${event.timestamp} ${event.level} [${event.context}] : " +
+                    "${event.logger} : ${event.message} : ${event.items}\n"
+        }
+    }
+})
