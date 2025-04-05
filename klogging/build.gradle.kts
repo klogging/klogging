04f9f74f84d7cf93f7ad810719c67304617a6b10
Michael Strasser <michael@michaelstrasser.com>
2025-04-05 13:02:57 +1000

    Sonar analysis in CI

--- klogging/build.gradle.kts
+++ klogging/build.gradle.kts
@@ -29,6 +29,7 @@ plugins {
     alias(libs.plugins.binaryCompatibilityValidator)
     alias(libs.plugins.testLogger)
     alias(libs.plugins.kover)
+    alias(libs.plugins.sonarqube)
 
     alias(libs.plugins.android.library)
 }
@@ -118,5 +119,13 @@ android {
 
     defaultConfig {
         minSdk = 21
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "klogging_klogging")
+        property("sonar.organization", "klogging")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
