845e863898297361a18bbd96cc2a7c93b9e8e6e8
Michael Strasser <michael@michaelstrasser.com>
2025-06-09 10:21:20 +1000

    Playpen formatting update

--- klogging/src/jvmTest/kotlin/io/klogging/Playpen.kt
+++ klogging/src/jvmTest/kotlin/io/klogging/Playpen.kt
@@ -50,109 +50,110 @@ fun localNow(): LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.curr
 /**
  * Main program for experimenting with Klogging features as they are developed.
  */
-suspend fun main() = coroutineScope {
-    val logger = logger("io.klogging.example.KloggerPlaypen", "source" to "Playpen")
-    Context.addBaseContext("app" to "Playpen")
+suspend fun main() =
+    coroutineScope {
+        val logger = logger("io.klogging.example.KloggerPlaypen", "source" to "Playpen")
+        Context.addBaseContext("app" to "Playpen")
 
-    if (System.getenv("RENDER_PATTERN") == "true") {
-        loggingConfiguration(append = false) {
-            sink(
-                "console",
-                RenderPattern("%t{LOCAL_TIME} %-5v{COLOUR} [%-20c] - %20l - %m - %i%4s"),
-                STDOUT
-            )
-            logging {
-                fromMinLevel(Level.TRACE) { toSink("console") }
-            }
-        }
-    }
-
-    noCoLogger("OneOffLogger", logger).info("Here it is", mapOf("black" to "white", "up" to "down"))
-
-    if (System.getenv("LOCAL_SPLUNK") == "true") {
-        loggingConfiguration(append = true) {
-            sink(
-                "splunk",
-                renderHec(source = "Playpen"),
-                splunkServer(
-                    hecUrl = "https://localhost:8088",
-                    hecToken = getenv("SPLUNK_HEC_TOKEN")!!,
-                    checkCertificate = false,
-                ),
-            )
-            logging {
-                fromMinLevel(Level.INFO) {
-                    toSink("splunk")
-                }
-            }
-        }
-    }
-
-    if (System.getenv("NEW_SEQ_CONFIG") == "true") {
-        loggingConfiguration {
-            sink(
-                "seq",
-                RENDER_CLEF,
-                seqServer(
-                    url = getenv("SEQ_URL")!!,
-                    apiKey = getenv("SEQ_API_KEY"),
-                    checkCertificate = false,
-                ),
-            )
-            logging {
-                toSink("seq")
-            }
-        }
-    }
-
-    if (System.getenv("ELK_CONFIG") == "true") {
-        loggingConfiguration {
-            sink(
-                "elk",
-                SendElk(
-                    ElkEndpoint(
-                        url = getenv("ELK_URL")!!,
-                        checkCertificate = false,
-                    )
+        if (System.getenv("RENDER_PATTERN") == "true") {
+            loggingConfiguration(append = false) {
+                sink(
+                    "console",
+                    RenderPattern("%t{LOCAL_TIME} %-5v{COLOUR} [%-20c] - %20l - %m - %i%4s"),
+                    STDOUT,
                 )
-            )
-        }
-    }
-
-    if (System.getenv("FUNC_INTERFACE_CONFIG") == "true") {
-        val logFile = File("/tmp/playpen.log")
-        val backwards = RenderString { it.evalTemplate().reversed() }
-        val tempFile = SendString { logFile.appendText("$it\n") }
-
-        loggingConfiguration(append = true) {
-            sink("tempLog", backwards, tempFile)
-            logging { toSink("tempLog") }
-        }
-    }
-
-    val outerCount = 3
-    val innerCount = 3
-
-    val run = randomUUID()
-    launch(logContext("run" to run) + CoroutineName("Playpen")) {
-        logger.info { "Start" }
-        repeat(outerCount) { c ->
-            logger.info { e(">> {Counter}", c + 1) }
-            launch(logContext("Counter" to (c + 1))) {
-                repeat(innerCount) { i ->
-                    logger.info("Event {Iteration} at {RightNow}", i + 1, localNow())
+                logging {
+                    fromMinLevel(Level.TRACE) { toSink("console") }
                 }
             }
-            delay(Random.nextLong(100))
-            logger.info { e("<< {Counter}", c + 1) }
-            functionWithException(logger)
         }
-        logger.info { "Finish" }
+
+        noCoLogger("OneOffLogger", logger).info("Here it is", mapOf("black" to "white", "up" to "down"))
+
+        if (System.getenv("LOCAL_SPLUNK") == "true") {
+            loggingConfiguration(append = true) {
+                sink(
+                    "splunk",
+                    renderHec(source = "Playpen"),
+                    splunkServer(
+                        hecUrl = "https://localhost:8088",
+                        hecToken = getenv("SPLUNK_HEC_TOKEN")!!,
+                        checkCertificate = false,
+                    ),
+                )
+                logging {
+                    fromMinLevel(Level.INFO) {
+                        toSink("splunk")
+                    }
+                }
+            }
+        }
+
+        if (System.getenv("NEW_SEQ_CONFIG") == "true") {
+            loggingConfiguration {
+                sink(
+                    "seq",
+                    RENDER_CLEF,
+                    seqServer(
+                        url = getenv("SEQ_URL")!!,
+                        apiKey = getenv("SEQ_API_KEY"),
+                        checkCertificate = false,
+                    ),
+                )
+                logging {
+                    toSink("seq")
+                }
+            }
+        }
+
+        if (System.getenv("ELK_CONFIG") == "true") {
+            loggingConfiguration {
+                sink(
+                    "elk",
+                    SendElk(
+                        ElkEndpoint(
+                            url = getenv("ELK_URL")!!,
+                            checkCertificate = false,
+                        ),
+                    ),
+                )
+            }
+        }
+
+        if (System.getenv("FUNC_INTERFACE_CONFIG") == "true") {
+            val logFile = File("/tmp/playpen.log")
+            val backwards = RenderString { it.evalTemplate().reversed() }
+            val tempFile = SendString { logFile.appendText("$it\n") }
+
+            loggingConfiguration(append = true) {
+                sink("tempLog", backwards, tempFile)
+                logging { toSink("tempLog") }
+            }
+        }
+
+        val outerCount = 3
+        val innerCount = 3
+
+        val run = randomUUID()
+        launch(logContext("run" to run) + CoroutineName("Playpen")) {
+            logger.info { "Start" }
+            repeat(outerCount) { c ->
+                logger.info { e(">> {Counter}", c + 1) }
+                launch(logContext("Counter" to (c + 1))) {
+                    repeat(innerCount) { i ->
+                        logger.info("Event {Iteration} at {RightNow}", i + 1, localNow())
+                    }
+                }
+                delay(Random.nextLong(100))
+                logger.info { e("<< {Counter}", c + 1) }
+                functionWithException(logger)
+            }
+            logger.info { "Finish" }
+        }
+        // There must be at least one statement outside the coroutine scope.
+        logger.info("All done", mapOf("run" to run))
+        delay(500)
     }
-    // There must be at least one statement outside the coroutine scope.
-    logger.info("All done", mapOf("run" to run))
-    delay(500)
-}
 
 suspend fun functionWithException(logger: Klogger) {
     try {
