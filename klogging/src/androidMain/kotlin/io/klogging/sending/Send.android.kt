130abf085695877607270900c88be5cc906cd5fd
Michael Strasser <michael@michaelstrasser.com>
2025-08-19 11:22:49 +1000

    Correct contributor name

--- klogging/src/androidMain/kotlin/io/klogging/sending/Send.android.kt
+++ klogging/src/androidMain/kotlin/io/klogging/sending/Send.android.kt
@@ -1,6 +1,6 @@
 /*
 
-   Copyright 2021-2025 Victor Kuzmin and Michael Strasser.
+   Copyright 2021-2025 Viktor Kuzmin and Michael Strasser.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
